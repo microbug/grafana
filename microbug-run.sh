63efd47a98160dbbe3611871135f43ff34cdee27
Richard Coleman <richard@microbug.uk>
2018-01-10 18:13:54 +0000

    Testing chown of all 104/107 files

--- microbug-run.sh
+++ microbug-run.sh
@@ -4,7 +4,7 @@ groupmod -g "$GID" grafana
 usermod -u "$UID" grafana
 chown -R grafana:grafana /var/lib/grafana
 
-# && `find / -user 104 -exec chown -h $UID {} \;` 
-# && `find / -group 107 -exec chgrp -h $GID {} \;`
+find / -user 104 -exec chown -h $UID {} \;
+find / -group 107 -exec chgrp -h $GID {} \;
 
 /run.sh
