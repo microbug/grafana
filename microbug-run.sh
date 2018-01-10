026914dc498b14c09c0a1363d841d40715a122c9
Richard Coleman <richard@microbug.uk>
2018-01-10 18:20:08 +0000

    More shellcheck compliance, suppress find errors

--- microbug-run.sh
+++ microbug-run.sh
@@ -4,7 +4,7 @@ groupmod -g "$GID" grafana
 usermod -u "$UID" grafana
 chown -R grafana:grafana /var/lib/grafana
 
-find / -user 104 -exec chown -h $UID {} \;
-find / -group 107 -exec chgrp -h $GID {} \;
+find / -user 104 -exec chown -h "$UID" {} \; 2>/dev/null
+find / -group 107 -exec chgrp -h "$GID" {} \; 2>/dev/null
 
 /run.sh
